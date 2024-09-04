6fe1268277d9551a09e19bb80c1f975c897ea098
Saqibi4213 <mehriamosawer@gmail.com>
2024-09-04 19:56:12 +0430

    add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import React from "react";
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+       <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Rockets />} />
+          <Route path="missions" element={<Missions />} />
+          <Route path="myprofile" element={<MyProfile />} />
+          <Route path="dragons" element={<Dragons />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
