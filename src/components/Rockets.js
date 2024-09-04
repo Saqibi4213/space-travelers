82dc65bb9f1f51f4bdbf2d13549958fb90f537ad
Saqibi4213 <mehriamosawer@gmail.com>
2024-09-04 21:24:17 +0430

    added files

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
+import '../modules/Rocket.css';
+import RocketsItem from './RocketsItem';
 
-export default function Rockets() {
+const Rockets = () => {
+  const selectedData = useSelector((state) => state.rockets);
+  const { loading, error, rocketData } = selectedData;
   return (
-    <div>Rockets</div>
+    <ul className="rocket-container">
+      {loading && <p>Loading</p>}
+      {error && <p>Error...</p>}
+      {!loading && !error
+        && rocketData.map((rocket) => (
+          <RocketsItem
+            key={rocket?.id}
+            id={rocket?.id}
+            name={rocket?.name}
+            image={rocket?.image}
+            description={rocket?.description}
+            reserved={rocket?.reserved}
+          />
+        ))}
+    </ul>
   );
-}
+};
+
+export default Rockets;
