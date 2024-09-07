92dfc4c9197de21f93c2fa5e00a6ad7929b99e85
Saqibi4213 <mehriamosawer@gmail.com>
2024-09-07 12:24:11 +0430

    added id for rockets

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,27 +1,37 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getDataFromServer } from '../redux/Rockets/RocketsSlice';
 import '../modules/Rocket.css';
 import RocketsItem from './RocketsItem';
 
 function Rockets() {
+  const dispatch = useDispatch();
   const selectedData = useSelector((state) => state.rockets);
   const { loading, error, rocketData } = selectedData;
 
+  useEffect(() => {
+    dispatch(getDataFromServer());
+  }, [dispatch]);
+
   return (
     <ul className="rocket-container">
-      {loading && <p>Loading</p>}
-      {error && <p>Error...</p>}
-      {!loading && !error
-        && rocketData.map((rocket) => (
-          <RocketsItem
-            key={rocket?.id}
-            id={rocket?.id}
-            name={rocket?.name}
-            image={rocket?.image}
-            description={rocket?.description}
-            reserved={rocket?.reserved}
-          />
-        ))}
+      {loading && <p>Loading...</p>}
+      {error && (
+        <p>
+          Error:
+          {error}
+        </p>
+      )}
+      {!loading && !error && rocketData.map((rocket) => (
+        <RocketsItem
+          key={rocket.id}
+          id={rocket.id}
+          name={rocket.name}
+          image={rocket.image}
+          description={rocket.description}
+          reserved={rocket.reserved}
+        />
+      ))}
     </ul>
   );
 }
--- src/components/RocketsItem.js
+++ src/components/RocketsItem.js
@@ -1,8 +1,12 @@
-import { Card, Button } from 'react-bootstrap';
+import React from 'react';
+import {
+  Card, Button, Row, Col,
+} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { reserveRocket } from '../redux/Rockets/RocketsSlice';
+import '../modules/Rocket.css';
 
 function RocketsItem({
   id, name, image, description, reserved,
@@ -14,21 +18,39 @@ function RocketsItem({
   };
 
   return (
-    <Card className="mb-3">
-      <Card.Img variant="top" src={image} alt={name} style={{ height: '200px', objectFit: 'cover' }} />
-      <Card.Body>
-        <Card.Title>{name}</Card.Title>
-        <Card.Text>
-          {reserved && <span className="badge bg-danger me-2">Reserved</span>}
-          {description}
-        </Card.Text>
-        <Button
-          variant={reserved ? 'danger' : 'primary'}
-          onClick={() => handleReserve(id)}
-        >
-          {reserved ? 'Cancel Reservation' : 'Reserve Rocket'}
-        </Button>
-      </Card.Body>
+    <Card className="mb-4 rocket-card">
+      <Row className="g-0">
+        <Col xs={12} md={4}>
+          <Card.Img
+            variant="top"
+            src={image}
+            alt={name}
+            className="img-fluid rocket-img"
+          />
+        </Col>
+        <Col xs={12} md={8}>
+          <Card.Body className="d-flex flex-column h-100">
+            <div className="d-flex flex-column flex-md-row justify-content-between align-items-start">
+              <Card.Title className="rocket-name mb-2 mb-md-0">{name}</Card.Title>
+              <Card.Text className="rocket-id mb-2 mb-md-0">
+                <strong>ID:</strong>
+                {id}
+              </Card.Text>
+            </div>
+            <Card.Text className="rocket-desc mt-3">
+              {reserved && <span className="badge bg-danger me-2">Reserved</span>}
+              {description}
+            </Card.Text>
+            <Button
+              variant={reserved ? 'danger' : 'primary'}
+              onClick={() => handleReserve(id)}
+              className="reserve-btn mt-3"
+            >
+              {reserved ? 'Cancel Reservation' : 'Reserve Rocket'}
+            </Button>
+          </Card.Body>
+        </Col>
+      </Row>
     </Card>
   );
 }
@@ -36,8 +58,8 @@ function RocketsItem({
 RocketsItem.propTypes = {
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
   reserved: PropTypes.bool.isRequired,
 };
 
