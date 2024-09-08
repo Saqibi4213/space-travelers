2932aa178dc75b2881833f9ba51dbcb7dc84f017
iamfarhatsharefi <sharefifarhat@gmail.com>
2024-09-08 02:27:22 -0700

    store.js code adding

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rocketsReducer from './Rockets/RocketsSlice';
+import missionsReducer from './missions/missionSlice';
 
 const store = configureStore({
   reducer: {
     rockets: rocketsReducer,
+    missions: missionsReducer,
   },
 });
 
