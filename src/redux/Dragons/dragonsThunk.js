2a55002f001db93692b8e1f79bd8cfc2beea29ae
Nooria150 <nooriamangal55@gmail.com>
2024-09-11 06:16:58 -0700

    added dragonThunk.js

---
+++ src/redux/Dragons/dragonsThunk.js
@@ -0,0 +1,11 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+// Define fetchDragons using createAsyncThunk
+export const fetchDragons = createAsyncThunk(
+  'dragons/fetchDragons',
+  async () => {
+    const response = await axios.get('https://api.example.com/dragons');
+    return response.data;
+  }
+);
